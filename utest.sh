102cf7ddb61bbd689c056578edb2865b3c8cd208
Jan D''Orgeville <jdorgeville@gmail.com>
2018-01-21 01:43:41 -0500

    test1.sh completed ! push/clone

--- utest.sh
+++ utest.sh
@@ -1,8 +1,37 @@
 #!/bin/sh
 set -e
-ROOT=`dirname $0`
-cd "$ROOT/utest"
+UTESTFOLDER=$(cd `dirname $0` | pwd)/utest
+OUTPUTPATH="$HOME/git-test.tmp"
+rm -rf $OUTPUTPATH
+mkdir -p $OUTPUTPATH
+cd $OUTPUTPATH
 
-./init.sh
+TESTTITLE_COLOR=$'\e[1;35m'
+TESTFAIL_COLOR=$'\e[1;37m\e[41m'
+TESTSUCCESS_COLOR=$'\e[1;37m\e[42m'
+CLEAR_COLOR=$'\e[0m'
 
-echo "success"
+startTest() {
+    local NAME=`basename $1 | sed 's/\.sh//g'`
+    echo $TESTTITLE_COLOR"[ "$NAME" ]"$CLEAR_COLOR
+    # create sand box folder
+    rm -rf $NAME; [ $? != 0 ] && exit 1
+    mkdir -p $NAME; [ $? != 0 ] && exit 1
+    pushd $NAME >/dev/null 2>&1 ;[ $? != 0 ] && exit 1
+    # execute test
+    $1 || exit 1
+    # test end
+    popd >/dev/null 2>&1; [ $? != 0 ] && exit 1
+    echo $TESTSUCCESS_COLOR"Success"$CLEAR_COLOR
+}
+
+{
+    find $UTESTFOLDER -name "*.sh" | while read f
+    do
+        startTest "$f"
+    done
+} || {
+    pwd
+    echo $TESTFAIL_COLOR"Failed"$CLEAR_COLOR
+    exit 1
+}
--- utest/test1.sh
+++ utest/test1.sh
@@ -1,14 +1,17 @@
+#!/bin/sh
+set -e
 git init --bare repo.git
-
 git template init
 git template unlock
 mkdir .template/repo/content/%%SRC%%/
 echo %%INPUT%% > .template/repo/content/%%SRC%%/%%MAIN%%.template
 git template add .
 git template commit -m "initial"
-
 git template remote add origin $(pwd)/repo.git
 git template remote -v
 git template push --set-upstream origin master
-
 git template lock
+git template remove
+git template clone repo.git
+
+
